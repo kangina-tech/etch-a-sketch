8cd38c600779e7aabe0dc5c263f4e4fe990a4c6c
Kangina <85179015+kangina-tech@users.noreply.github.com>
2024-04-04 14:14:54 +0300

    set up canvas for drawing on js

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,22 @@
 // select elements on page (canvas)
-const canvas = document.querySelector('etch-a-sketch');
+const canvas = document.getElementById('etch-a-sketch');
 const ctx = canvas.getContext('2d');
 const shakebutton = document.querySelector('.shake')
 
 // set up canvas for drawing
+const { width, height } = canvas;
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
+
 ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
+ctx.lineCap = 'square';
 ctx.lineWidth = 10;
+
+ctx.beginPath();
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
+
 
 // write a draw function
 
