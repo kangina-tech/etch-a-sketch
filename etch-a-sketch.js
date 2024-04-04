4e7af8fa5cede388a58456b0906d6d54dff80fbe
Kangina <85179015+kangina-tech@users.noreply.github.com>
2024-04-04 11:45:26 +0300

    completed html and css file components

---
+++ etch-a-sketch.js
@@ -0,0 +1,16 @@
+// select elements on page (canvas)
+const canvas = document.querySelector('etch-a-sketch');
+const ctx = canvas.getContext('2d');
+const shakebutton = document.querySelector('.shake')
+
+// set up canvas for drawing
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+// write a draw function
+
+// write handler for keys
+
+// clear or shake function
+
+// listen for arrowkeys
